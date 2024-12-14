8c71a976d7cd7d4d2647230c63a803f88b35ee28
Roman Shirasaki <jgdevromank@gmail.com>
2024-12-14 15:03:58 +0100

    sample treemap

--- frontend/src/components/LinePlot.tsx
+++ frontend/src/components/LinePlot.tsx
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import "./App.css";
+import { useEffect, useRef } from "react";
 
 interface LinePlotProps {
   data: number[];
@@ -20,14 +21,21 @@ export default function LinePlot({
   marginBottom = 20,
   marginLeft = 20,
 }: LinePlotProps) {
+  
+  const gx = useRef();
+  const gy = useRef();
   const x = d3.scaleLinear(
     [0, data.length - 1],
     [marginLeft, width - marginRight]
   );
-  const y = d3.scaleLinear(d3.extent(data) as [number, number], [height - marginBottom, marginTop]);
+  const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
   const line = d3.line((d, i) => x(i), y);
+  useEffect(() => void d3.select(gx.current).call(d3.axisBottom(x)), [gx, x]);
+  useEffect(() => void d3.select(gy.current).call(d3.axisLeft(y)), [gy, y]);
   return (
     <svg width={width} height={height}>
+      <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
+      <g ref={gy} transform={`translate(${marginLeft},0)`} />
       <path
         fill="none"
         stroke="currentColor"
